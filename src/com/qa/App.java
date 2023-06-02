1de78726b471ca77fd307922fafef00ff65bc061
GLeonigby <133762620+Gary-Leonard@users.noreply.github.com>
2023-06-02 16:21:46 +0100

    add save function

--- src/com/qa/App.java
+++ src/com/qa/App.java
@@ -1,8 +1,9 @@
 package com.qa;
 
 
+import com.qa.data.InMemoryDAO;
 import com.qa.data.Song;
-
+import com.qa.data.SongDAO;
 import java.time.LocalDate;
 
 public class App {
@@ -10,13 +11,28 @@ public class App {
     public static void main(String[] args) {
 
         Song song1 = new Song(1, "Frog Prince","Keane",261, LocalDate.of(2006,6,12));
-        Song song2 = new Song(2, "Song2","Blur",122, LocalDate.of(1997,6,8));
-        Song song3 = new Song(3, "Monkey Wrench","Foo Fighters",231, LocalDate.of(1997,4,12));
+        Song song2 = new Song(2, "Song2","Blur",122, LocalDate.of(1997,4,7));
+        Song song3 = new Song(3, "Monkey Wrench","Foo Fighters",231, LocalDate.of(1997,4,28));
 
-        System.out.println(song1);
-        System.out.println(song2);
-        System.out.println(song3);
 
+        Song[] data = new Song[] { song1, song2, song3 };
+        SongDAO dao = new InMemoryDAO(data);
+
+        for (Song song : dao.readAll()) {
+            System.out.println(song);
+        }
+        System.out.println("SONG WITH ID 2: ");
+        System.out.println(dao.readById(2));
+
+        System.out.println("Saving new song");
+        Song song4 = new Song(4, "Operation Blade", "Public Domain", 187,
+                LocalDate.of(2000,11,20));
+        dao.save(song4);
+
+        System.out.println("READ ALL SONGS:");
+        for (Song song : dao.readAll()) {
+            System.out.println(song);
+        }
 
     }
 }
