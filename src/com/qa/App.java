0543aaa06ce91ce9aad9f1bb21631abf61868e0d
GLeonigby <133762620+Gary-Leonard@users.noreply.github.com>
2023-06-02 14:57:48 +0100

    add correct duration

--- src/com/qa/App.java
+++ src/com/qa/App.java
@@ -9,9 +9,9 @@ public class App {
 
     public static void main(String[] args) {
 
-        Song song1 = new Song(1, "Frog Prince","Keane",300, LocalDate.of(2006,6,12));
+        Song song1 = new Song(1, "Frog Prince","Keane",261, LocalDate.of(2006,6,12));
         Song song2 = new Song(2, "Song2","Blur",122, LocalDate.of(1997,6,8));
-        Song song3 = new Song(3, "Monkey Wrench","Foo Fighters",280, LocalDate.of(1997,4,12));
+        Song song3 = new Song(3, "Monkey Wrench","Foo Fighters",231, LocalDate.of(1997,4,12));
 
         System.out.println(song1);
         System.out.println(song2);
@@ -19,6 +19,4 @@ public class App {
 
 
     }
-
-
 }
